8de5f758b4a8675ba4d4d7ce7c908b24c201007b
Raul Cordoba <raulc99g@gmail.com>
2025-01-14 18:20:27 -0600

    Define SignupRequest DTO with validation for user registration

---
+++ src/main/java/com/raulcg/ecommerce/request/SignupRequest.java
@@ -0,0 +1,24 @@
+package com.raulcg.ecommerce.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+
+    @NotBlank
+    @Size(min = 4, max = 100)
+    private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 40)
+    private String password;
+
+}
