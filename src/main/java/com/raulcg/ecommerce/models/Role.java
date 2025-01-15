1ed6dc09bc775326f60bf9e33d92d9f26d947b89
Raul Cordoba <raulc99g@gmail.com>
2025-01-14 18:18:47 -0600

    Create Role entity with relationship to User and role type

---
+++ src/main/java/com/raulcg/ecommerce/models/Role.java
@@ -0,0 +1,32 @@
+package com.raulcg.ecommerce.models;
+
+import com.raulcg.ecommerce.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UserRole role;
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    private Set<User> users = new HashSet<>();
+
+    public Role(UserRole roleName) {
+        this.role = roleName;
+    }
+
+}
