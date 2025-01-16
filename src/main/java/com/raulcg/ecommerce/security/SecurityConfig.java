ebd18cbe46d9411fa61ec358a00206aa0ced3c97
Raul Cordoba <raulc99g@gmail.com>
2025-01-16 12:37:18 -0600

    Integrate SecurityFilterChain into SecurityConfig for stateless session management

--- src/main/java/com/raulcg/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/raulcg/ecommerce/security/SecurityConfig.java
@@ -7,12 +7,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Autowired
     private RoleRepository roleRepository;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 
     @Bean
     public CommandLineRunner initData(RoleRepository roleRepository) {
