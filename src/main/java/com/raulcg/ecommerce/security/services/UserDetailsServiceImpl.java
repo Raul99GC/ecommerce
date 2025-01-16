e53ae1b64b0d3517d18d2e432b88d7f1536196ac
Raul Cordoba <raulc99g@gmail.com>
2025-01-16 00:00:31 -0600

    Add UserDetailsServiceImpl for loading user details by username

---
+++ src/main/java/com/raulcg/ecommerce/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.raulcg.ecommerce.security.services;
+
+import com.raulcg.ecommerce.models.User;
+import com.raulcg.ecommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    // UserDetailsService, esta interfaz se utiliza para cargar datos del usuario
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return UserDetailsImpl.build(user);
+    }
+
+}
