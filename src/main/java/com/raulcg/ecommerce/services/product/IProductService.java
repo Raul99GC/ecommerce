9bab290e78215dd46f59b241ed2f7478836c77d2
Raul Cordoba <raulc99g@gmail.com>
2025-01-19 15:20:25 -0600

    Define IProductService interface for product operations

---
+++ src/main/java/com/raulcg/ecommerce/services/product/IProductService.java
@@ -0,0 +1,22 @@
+package com.raulcg.ecommerce.services.product;
+
+import com.raulcg.ecommerce.models.Product;
+import com.raulcg.ecommerce.request.ProductRequest;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IProductService {
+
+    String handleImageUpload(MultipartFile file) throws Exception;
+
+    void createProduct(ProductRequest product);
+
+    List<Product> getAllProducts();
+
+    void editProductById(ProductRequest product, UUID productId);
+
+    void deleteProductById(UUID productId);
+
+}
