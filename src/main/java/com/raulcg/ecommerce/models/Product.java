4b73d04f9f2d7d5e0dfbeb86d1f20b598eac8fbb
Raul Cordoba <raulc99g@gmail.com>
2025-01-19 15:19:07 -0600

    Define Product entity with necessary fields

---
+++ src/main/java/com/raulcg/ecommerce/models/Product.java
@@ -0,0 +1,47 @@
+package com.raulcg.ecommerce.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String image;
+
+    private String title;
+
+    private String description;
+
+    private String category;
+
+    private String brand;
+
+    private double price;
+
+    private int stock;
+
+    private int averageReview;
+
+    @JsonIgnore
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @JsonIgnore
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
