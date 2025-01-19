3640f5d70dc42aee8d7161a070416bf7a81bfd97
Raul Cordoba <raulc99g@gmail.com>
2025-01-19 15:21:10 -0600

    Add LoginRequest DTO with validation annotations

---
+++ src/main/java/com/raulcg/ecommerce/request/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.raulcg.ecommerce.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @NotBlank
+    @Email
+    @Size(max = 50)
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+}
