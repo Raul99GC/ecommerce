37941e92ba0951754b7727ae4d364d7e1d7527da
Raul Cordoba <raulc99g@gmail.com>
2025-01-19 21:24:31 -0600

    Update to load user by email instead of username

--- src/main/java/com/raulcg/ecommerce/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/raulcg/ecommerce/security/services/UserDetailsServiceImpl.java
@@ -16,8 +16,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUserName(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("User not found"));
         return UserDetailsImpl.build(user);
     }
 
