514056c0fba7d33415c785f234ea86d8d76cdcb1
Raul Cordoba <raulc99g@gmail.com>
2025-01-21 13:35:21 -0600

    Update SecurityConfig to include JWT authentication, authorization rules, and a password encoder.

--- src/main/java/com/raulcg/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/raulcg/ecommerce/security/SecurityConfig.java
@@ -1,28 +1,64 @@
 package com.raulcg.ecommerce.security;
 
-import com.raulcg.ecommerce.enums.UserRole;
-import com.raulcg.ecommerce.models.Role;
-import com.raulcg.ecommerce.repositories.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.raulcg.ecommerce.enums.UserRole;
+import com.raulcg.ecommerce.models.Role;
+import com.raulcg.ecommerce.repositories.RoleRepository;
+import com.raulcg.ecommerce.security.jwt.AuthEntryPointJwt;
+import com.raulcg.ecommerce.security.jwt.AuthTokenFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Autowired
-    private RoleRepository roleRepository;
+    private AuthTokenFilter authenticationJwtTokenFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.sessionManagement(
+                managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .anyRequest().permitAll()
+        );
+
+        http.addFilterBefore(authenticationJwtTokenFilter, BasicAuthenticationFilter.class);
+        http.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler));
+
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+    // ? Crea un bean del tipo AuthenticationManager para que esté disponible en el contexto de Spring y pueda ser utilizado en otros lugares de tu aplicación.
+    // ? Se utiliza para gestionar el proceso de autenticación en la aplicación.
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
     }
 
     @Bean
