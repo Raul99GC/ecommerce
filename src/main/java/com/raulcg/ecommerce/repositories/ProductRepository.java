a77f6ddc6255e58475c41f0e8675bdbd8dc1ea83
Raul Cordoba <raulc99g@gmail.com>
2025-01-21 13:35:51 -0600

    Enhance ProductRepository to support filtered product queries with dynamic sorting.

--- src/main/java/com/raulcg/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/raulcg/ecommerce/repositories/ProductRepository.java
@@ -2,8 +2,24 @@ package com.raulcg.ecommerce.repositories;
 
 import com.raulcg.ecommerce.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    @Query("SELECT p FROM Product p " +
+            "WHERE (:categories IS NULL OR p.category IN :categories) " +
+            "AND (:brands IS NULL OR p.brand IN :brands) " +
+            "ORDER BY " +
+            "CASE WHEN :sortBy = 'price-lowtohigh' THEN p.price END ASC, " +
+            "CASE WHEN :sortBy = 'price-hightolow' THEN p.price END DESC, " +
+            "CASE WHEN :sortBy = 'title-atoz' THEN p.title END ASC, " +
+            "CASE WHEN :sortBy = 'title-ztoa' THEN p.title END DESC")
+    List<Product> findFilteredProducts(
+            @Param("categories") List<String> categories,
+            @Param("brands") List<String> brands,
+            @Param("sortBy") String sortBy
+    );
 }
