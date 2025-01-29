0f93e4ed5d5b122499fa91aed3cb976e792a32c0
Raul Cordoba <raulc99g@gmail.com>
2025-01-29 14:49:32 -0600

    Define interface for cart service operations

---
+++ src/main/java/com/raulcg/ecommerce/services/cart/ICartService.java
@@ -0,0 +1,19 @@
+package com.raulcg.ecommerce.services.cart;
+
+import com.raulcg.ecommerce.models.Cart;
+import com.raulcg.ecommerce.request.CartRequest;
+
+import java.util.UUID;
+
+public interface ICartService {
+
+    // add product to cart
+    Cart addProductToCart(CartRequest cartRequest);
+
+    Cart getCart(UUID userId);
+
+    Cart updateCartItem(CartRequest cartRequest);
+
+    Cart deleteCartItem(CartRequest cartRequest);
+
+}
