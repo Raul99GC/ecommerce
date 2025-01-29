4242e85c4ead0c47f48d2f8a3e4cf4536dcab2ba
Raul Cordoba <raulc99g@gmail.com>
2025-01-29 14:51:56 -0600

    Add CartItem repository interface for database operations

---
+++ src/main/java/com/raulcg/ecommerce/repositories/CartItemRepository.java
@@ -0,0 +1,8 @@
+package com.raulcg.ecommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
+}
