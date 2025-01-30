b0c83a5386beaa65cee52b8563163a81f1e41982
Raul Cordoba <raulc99g@gmail.com>
2025-01-29 20:09:01 -0600

    Enriched ProductRequest with @NotBlank annotations for brand and category properties

--- src/main/java/com/raulcg/ecommerce/request/ProductRequest.java
+++ src/main/java/com/raulcg/ecommerce/request/ProductRequest.java
@@ -1,17 +1,34 @@
 package com.raulcg.ecommerce.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class ProductRequest {
-    private String title;
-    private String description;
-    private double price;
-    private String image;
-    private String category;
-    private String brand;
-    private int stock;
+
     private int averageReview;
+
+    @NotBlank(message = "brand is required")
+    private String brand;
+    @NotBlank(message = "category is required")
+    private String category;
+    @NotBlank(message = "description is required")
+    private String description;
+    @NotBlank(message = "image is required")
+    private String image;
+    @NotBlank(message = "Name is required")
+
+    @Min(value = 0, message = "salePrice must be greater than 0")
+    private int salePrice;
+
+    @NotBlank(message = "salePrice is required")
+    @Min(value = 0, message = "price must be greater than 0")
+    private double price;
+    @NotBlank(message = "title is required")
+    private String title;
+    @NotBlank(message = "stock is required")
+    private int stock;
 }
