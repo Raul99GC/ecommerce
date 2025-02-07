f2a8d17d056e1a8f8ecd6374b4a9a65a62896686
Raul Cordoba <raulc99g@gmail.com>
2025-02-07 00:41:30 -0600

    Create Feature entity with UUID, image field.

---
+++ src/main/java/com/raulcg/ecommerce/models/Feature.java
@@ -0,0 +1,34 @@
+package com.raulcg.ecommerce.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feature {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String image;
+
+    @JsonIgnore
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @JsonIgnore
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
