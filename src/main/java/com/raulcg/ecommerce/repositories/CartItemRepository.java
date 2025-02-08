1e9a55fb8f7a4e5b5b109548b054381f44cb4d23
Raul Cordoba <raulc99g@gmail.com>
2025-02-08 16:22:36 -0600

    Add repository method to delete cart items by user ID

--- src/main/java/com/raulcg/ecommerce/repositories/CartItemRepository.java
+++ src/main/java/com/raulcg/ecommerce/repositories/CartItemRepository.java
@@ -1,8 +1,10 @@
 package com.raulcg.ecommerce.repositories;
 
+import com.raulcg.ecommerce.models.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
+    void deleteByCart_userId(UUID userId);
 }
