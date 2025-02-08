84a922209ed18f5ac70d40c84549de8b242b47b3
Raul Cordoba <raulc99g@gmail.com>
2025-02-08 17:20:03 -0600

    Add message field and extended constructor to GenericApiResponse

--- src/main/java/com/raulcg/ecommerce/responses/GenericApiResponse.java
+++ src/main/java/com/raulcg/ecommerce/responses/GenericApiResponse.java
@@ -7,10 +7,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class GenericApiResponse<T> {
     private boolean success;
+    private String message;
     private T data;
 
     public GenericApiResponse(boolean success, T data) {
         this.success = success;
         this.data = data;
+    }
+    public GenericApiResponse(boolean success, T data, String message) {
+        this.success = success;
+        this.data = data;
+        this.message = message;
     }
 }
